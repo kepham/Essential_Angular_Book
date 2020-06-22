e07b42a968f9fd6b755655a4b0a0ea287402236b
kephamose <kepham@gatech.edu>
2020-06-22 08:09:20 -0500

    chapter 8

---
+++ SportsStore/ClientApp/src/app/store/ratings.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { Product } from "../models/product.model";
+@Component({
+    selector: "store-ratings",
+    templateUrl: "ratings.component.html"
+})
+export class RatingsComponent {
+
+}
--- SportsStore/ServerApp/Startup.cs
+++ SportsStore/ServerApp/Startup.cs
@@ -72,7 +72,7 @@ namespace ServerApp
 
                 endpoints.MapControllerRoute(
                     name: "angular_fallback",
-                    pattern: "{target:regex(table|detail)}/{*catchall}",
+                    pattern: "{target:regex(store)}/{*catchall}",
                     defaults: new { controller = "Home", action = "Index" });
 
                 endpoints.MapRazorPages();
